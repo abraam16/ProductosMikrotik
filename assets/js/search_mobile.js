483a8f0fee77537126b4abead33cea29e26910bd
abraam16 <u202219199@upc.edu.pe>
2025-08-02 15:24:26 -0500

    feat

--- assets/js/search_mobile.js
+++ assets/js/search_mobile.js
@@ -1,5 +1,4 @@
 // search_mobile.js
-// Controla la visibilidad del buscador móvil (sec_search)
 document.addEventListener('DOMContentLoaded', function() {
   // Eliminado: la lógica de la lupa y buscador ahora está en nav.js para evitar conflictos
 });
